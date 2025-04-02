3397f04e6897a0ce315eae54bdcef947a4be0b77
robcarmof <robcarmof@gmail.com>
2025-04-02 16:03:37 -0300

    creating modules

--- cluster/security-groups.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_security_group_rule" "demo-node-ingress-http" {
-  description              = "Allow HTTP inbound"
-  from_port                = 80
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.demo-node.id
-  to_port                  = 80
-  type                     = "ingress"
-  cidr_blocks              = ["0.0.0.0/0"]
-}
