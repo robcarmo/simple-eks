996dfc84ab09a71694e242a7071ddcd5dabb7115
robcarmo <33102681+robcarmo@users.noreply.github.com>
2025-04-02 16:38:11 -0300

    Add VPC module and update EKS module dependencies
    Add VPC module and update EKS module dependencies

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,11 +1,7 @@
-data "aws_availability_zones" "available" {
-  state = "available"
-}
+data "aws_availability_zones" "available" {}
 
 resource "aws_vpc" "main" {
-  cidr_block           = var.vpc_cidr
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+  cidr_block = var.vpc_cidr
 
   tags = merge(
     {
@@ -18,16 +14,15 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public" {
   count = var.num_azs
 
-  availability_zone       = data.aws_availability_zones.available.names[count.index]
-  cidr_block             = cidrsubnet(var.vpc_cidr, 8, count.index)
-  vpc_id                 = aws_vpc.main.id
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index)
+  vpc_id            = aws_vpc.main.id
   map_public_ip_on_launch = true
 
   tags = merge(
     {
       "Name" = "${var.name_prefix}-public-subnet-${data.aws_availability_zones.available.names[count.index]}"
       "kubernetes.io/role/elb" = "1"
-      "kubernetes.io/cluster/${var.cluster_name_tag}" = "shared"
     },
     var.tags
   )
