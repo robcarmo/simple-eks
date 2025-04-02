b732d7ccd67b32350279b11b952c98e87a8506ea
robcarmo <33102681+robcarmo@users.noreply.github.com>
2025-04-02 18:37:15 -0300

    Refactor EKS module configuration and improve documentation
    Refactor EKS module configuration and improve documentation

--- modules/eks/templates/userdata.sh
+++ modules/eks/templates/userdata.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-echo "Configuring node..."
-curl -o /etc/eks/bootstrap.sh https://amazon-eks.s3.us-west-2.amazonaws.com/${CLUSTER_NAME}/bootstrap.sh
-chmod +x /etc/eks/bootstrap.sh
-/etc/eks/bootstrap.sh ${CLUSTER_NAME} --kubelet-extra-args "--node-labels=node-type=worker"
+set -o xtrace
+/etc/eks/bootstrap.sh ${CLUSTER_NAME}
