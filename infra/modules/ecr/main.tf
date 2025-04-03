7c14150d484644165abb1c21c4dd7bdbb9dd812b
robcarmo <robcarmof@gmail.com>
2025-04-03 18:59:12 -0300

    fixing back

--- infra/modules/ecr/main.tf
+++ infra/modules/ecr/main.tf
@@ -3,13 +3,13 @@ data "aws_ecr_repository" "ecr_repo" {
 }
 
 
-resource "aws_ecr_repository" "ecr_repo" {
-  name                 = var.repository_name
-  image_tag_mutability = var.image_tag_mutability ? "MUTABLE" : "IMMUTABLE"
+// resource "aws_ecr_repository" "ecr_repo" {
+//   name                 = var.repository_name
+//   image_tag_mutability = var.image_tag_mutability ? "MUTABLE" : "IMMUTABLE"
 
-  image_scanning_configuration {
-    scan_on_push = var.scan_images_on_push
-  }
+//   image_scanning_configuration {
+//     scan_on_push = var.scan_images_on_push
+//   }
 
-  tags = var.tags
-}
+//   tags = var.tags
+// }
