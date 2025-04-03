eab3d88297654135b42f56c9a6f7c8b40c99f10b
robcarmo <33102681+robcarmo@users.noreply.github.com>
2025-04-03 12:21:41 -0300

    Replace Kubernetes manifests with Helm chart structure
    Replace Kubernetes manifests with Helm chart structure

--- kubernetes/variables.tf
+++ kubernetes/variables.tf
@@ -1,14 +1,1 @@
-variable "namespace_name" {
-  default = "demo-service"
-  type    = "string"
-}
 
-variable "nginx_pod_name" {
-  default = "demo-service"
-  type    = "string"
-}
-
-variable "nginx_pod_image" {
-  default = "nginx:latest"
-  type    = "string"
-}
