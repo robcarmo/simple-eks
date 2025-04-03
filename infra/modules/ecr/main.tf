2c65f35ee0c1624560a82b530f5572952d5340bb
robcarmo <robcarmof@gmail.com>
2025-04-03 18:12:02 -0300

    fixing ecr

--- infra/modules/ecr/main.tf
+++ infra/modules/ecr/main.tf
@@ -1,13 +1,14 @@
+data "aws_ecr_repository" "ecr_repo" {
+  name = var.repository_name
+}
+
+
 resource "aws_ecr_repository" "ecr_repo" {
   name                 = var.repository_name
   image_tag_mutability = var.image_tag_mutability ? "MUTABLE" : "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = var.scan_images_on_push
-  }
-
-  lifecycle {
-    ignore_changes = [/* list attributes you want Terraform to ignore */]
   }
 
   tags = var.tags
--- infra/modules/ecr/outputs.tf
+++ infra/modules/ecr/outputs.tf
@@ -1,14 +1,15 @@
+
 output "repository_url" {
   description = "The URL of the ECR repository."
-  value       = aws_ecr_repository.ecr_repo.repository_url
+  value       = data.aws_ecr_repository.ecr_repo.repository_url
 }
 
 output "repository_arn" {
   description = "The ARN of the ECR repository."
-  value       = aws_ecr_repository.ecr_repo.arn
+  value       = data.aws_ecr_repository.ecr_repo.arn
 }
 
 output "repository_name" {
   description = "The name of the ECR repository."
-  value       = aws_ecr_repository.ecr_repo.name
+  value       = data.aws_ecr_repository.ecr_repo.name
 }
