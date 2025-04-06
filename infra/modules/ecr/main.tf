e4009d0c4dea4737cb4922b13bbbb1f022ae6c09
robcarmo <robcarmof@gmail.com>
2025-04-06 16:48:42 -0300

    fixing image

--- infra/modules/ecr/main.tf
+++ infra/modules/ecr/main.tf
@@ -8,7 +8,7 @@
 # Resource block is now active (uncommented)
 resource "aws_ecr_repository" "ecr_repo" {
   name                 = var.repository_name
-  image_tag_mutability = var.image_tag_mutability ? "MUTABLE" : "IMMUTABLE"
+  image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
     scan_on_push = var.scan_images_on_push
