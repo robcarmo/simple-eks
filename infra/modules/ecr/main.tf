8095df926ba393d221ea8b5c778d6e6d74af964d
robcarmo <robcarmof@gmail.com>
2025-04-06 18:41:14 -0300

    fixing ecr

--- infra/modules/ecr/main.tf
+++ infra/modules/ecr/main.tf
@@ -9,7 +9,7 @@
 resource "aws_ecr_repository" "ecr_repo" {
   name                 = var.repository_name
   image_tag_mutability = "MUTABLE"
-
+  force_delete         = true
   image_scanning_configuration {
     scan_on_push = var.scan_images_on_push
   }
